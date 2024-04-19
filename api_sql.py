dde18b01f7cf14deac6a772193d2019f25b5cada
Dorian Rakotonanahary <79524563+Drakoton@users.noreply.github.com>
2024-04-19 15:46:16 +0200

    Update api_sql.py with comments

--- api_sql.py
+++ api_sql.py
@@ -1,3 +1,5 @@
+# On importe les différents modules nécessaires pour l'exercice
+
 from sqlalchemy import create_engine, text
 from faker import Faker
 from datetime import datetime
@@ -6,13 +8,13 @@ from jinja2 import escape
 from jinja2.filters import escape
 import random 
 
-db_string = "postgresql://root:root@localhost:5432/postgres"
+db_string = "postgresql://root:root@localhost:5432/postgres" #On utilise la BDD postgres de Pgadmin
 
 engine = create_engine(db_string)
 
 app = Flask(__name__)
 
-@app.route("/user", methods=["GET"])
+@app.route("/user", methods=["GET"]) #On va utiliser la méthode GET pour récupérer les infos de chaque ligne de la BDD
 def get_users():
     users = run_sql_with_result("SELECT * FROM users")
     data = []
@@ -26,7 +28,7 @@ def get_users():
             "job": row[5],
         }
         data.append(user)
-    return jsonify(data)
+    return jsonify(data) on retourne sur le web les différents lignes sous format document JSON qu'on peut ensuite télécharger
     #[
     #     {
     #         "id":1,
@@ -42,9 +44,11 @@ def get_users():
     # ]
     # return users
 
-fake = Faker()
+fake = Faker() # Faker va nous servir à créer de la data pour les différetes colonnes
 
-create_user_table_sql = """
+
+# On va maintenant créer les 2 tables users et Application
+create_user_table_sql = """ 
 CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     firstname VARCHAR(100),
@@ -63,14 +67,14 @@ CREATE TABLE IF NOT EXISTS Application (
     user_id INTEGER REFERENCES users(id)
 );
 """
-def run_sql(query: str):
+def run_sql(query: str):     # la fonction pour créer les tables
     with engine.connect() as connection:
         trans = connection.begin()
         connection.execute(text(query))
         trans.commit()
 
 
-def run_sql_with_result(query: str):
+def run_sql_with_result(query: str): 
     with engine.connect() as connection:
         trans = connection.begin()
         result = connection.execute(text(query))
@@ -78,10 +82,10 @@ def run_sql_with_result(query: str):
         return result 
 
 
-def populate_tables():
-    apps = ["Facebook","Instagram","TikTok","Twitter"]
+def populate_tables(): # la fonction pour créer les lignes pour les tables
+    apps = ["Facebook","Instagram","TikTok","Twitter"] # les différentes applications des utilisateurs
     for _ in range(100):
-        firstname = fake.first_name()
+        firstname = fake.first_name() #on utilise faker pour les différentes colonnes de users
         lastname = fake.last_name()
         age = random.randrange(18, 50)
         email = fake.email()
@@ -93,7 +97,7 @@ def populate_tables():
             VALUES ('{firstname}','{lastname}','{age}','{email}','{job}')
             RETURNING id
         """
-        user_id = run_sql_with_result(insert_user_query).scalar()
+        user_id = run_sql_with_result(insert_user_query).scalar() 
 
 
         num_apps = random.randint(1,5)
@@ -104,13 +108,13 @@ def populate_tables():
 
             sql_insert_app = f"""
                 INSERT INTO applications (appname, username, lastconnection, user_id)
-                VALUES ('{app_name}','{username}','{lastconnection}','{user_id}')
+                VALUES ('{app_name}','{username}','{lastconnection}','{user_id}') #user_id est la foreign key de users
                 RETURNING id
             """
         
             run_sql(insert_user_query)
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Enfin on print les fonctions avec la définition du port qu'on va utiliser pour l'affichage de la page web avec les colonnes
     with app.app_context():
         run_sql(create_user_table_sql)
         run_sql(create_application_table_sql)
